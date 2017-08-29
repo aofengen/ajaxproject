36b5e07eca16da1ce7cce4b1baf1129ecc9f260a
Aaron Ofengender <aofengen@gmail.com>
2017-08-29 14:54:32 -0400

    project deployed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(reg,res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 })
 
 http.listen(3000, function(){
